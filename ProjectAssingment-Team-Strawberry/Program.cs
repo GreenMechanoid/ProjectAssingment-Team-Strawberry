c1a5127cdd4a1665f098208b4189e823be34a20c
Daniel Svensson <daniel.svensson682@gmail.com>
2022-11-07 18:19:41 +0100

    Add project files.

---
+++ ProjectAssingment-Team-Strawberry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectAssingment_Team_Strawberry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
