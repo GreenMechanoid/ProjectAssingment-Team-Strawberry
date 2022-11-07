e48a5ead1cd297f5610a5e6756e0ca9833469172
Daniel Svensson <daniel.svensson682@gmail.com>
2022-11-07 22:34:12 +0100

    made all "adjustable" parts refer to 'this' variable

--- ProjectAssingment-Team-Strawberry/Menus.cs
+++ ProjectAssingment-Team-Strawberry/Menus.cs
@@ -6,97 +6,98 @@ using System.Threading;
 
 namespace ProjectAssingment_Team_Strawberry
 {
-    internal class Menus
-    {
-        int menuChoise = 0;
-        bool loopIsRunning = true;
-        bool isAnInt = false;
-        public void startMenu()
-        {
-            this.menuChoise = 0;
-            loopIsRunning = true;
-            // creating a userInterface with a Switch case
-            do
-            {
-                switch (menuChoise)
-                {
-                    default: // default is user's choices, writeline messages with the choices available.
+	internal class Menus
+	{
+		int menuChoise = 0;
+		bool loopIsRunning = true;
+		bool isAnInt = false;
+		public void startMenu()
+		{
+			this.menuChoise = 0;
+			this.loopIsRunning = true;
+			// creating a userInterface with a Switch case
+			do
+			{
+				switch (this.menuChoise)
+				{
+					default: // default is user's choices, writeline messages with the choices available.
 
-                        Console.Clear();
-                        Console.WriteLine("1: User management");
-                        Console.WriteLine("99: Terminate Program");
-                        do
-                        {
-                            int.TryParse(Console.ReadLine(), out menuChoise);
-                            //simple if to check that the number is corrisponding to a 'Menu Item'
-                            if (menuChoise == 1 || menuChoise == 99)  // ** expand with the numbers of the menu items
-                            {
-                                isAnInt = true;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Wrong input! , Can't find a matching menu item!");
-                            }
-                        } while (!isAnInt); //keep looping if it's not a number
-                        break;
-                    case 1:
-                        UserManagement();
-                        break;
-                    case 99: //Termination Selection
-                        loopIsRunning = false; // to terminate the do while after choice is to terminate the program
-                        Console.Clear();
-                        Console.WriteLine("Thank you for running this program. have a nice day!");
-                        break;
+						Console.Clear();
+						Console.WriteLine("1: User management");
+						Console.WriteLine("99: Terminate Program");
+						do
+						{
+							int.TryParse(Console.ReadLine(), out this.menuChoise);
+							//simple if to check that the number is corrisponding to a 'Menu Item'
+							if (this.menuChoise == 1 || this.menuChoise == 99)  // ** expand with the numbers of the menu items
+							{
+								this.isAnInt = true;
+							}
+							else
+							{
+								Console.WriteLine("Wrong input! , Can't find a matching menu item!");
+							}
+						} while (!this.isAnInt); //keep looping if it's not a number
+						break;
+					case 1:
+						UserManagement();
+						break;
+					case 99: //Termination Selection
+						this.loopIsRunning = false; // to terminate the do while after choice is to terminate the program
+						Console.Clear();
+						Console.WriteLine("Thank you for running this program. have a nice day!");
+						break;
 
-                }
+				}
 
-            } while (loopIsRunning);
-        }
-        public void UserManagement()
-        {
-            this.menuChoise = 0;
-            this.loopIsRunning = true;
-            do
-            {
-            switch (menuChoise)
-            {
-                default:
-                    Console.Clear();
-                    Console.WriteLine("1: Create User");
-                    Console.WriteLine("2: Login User");
-                    Console.WriteLine("3: Return to Previous Menu");
+			} while (this.loopIsRunning);
+		}
+		public void UserManagement()
+		{
+			this.menuChoise = 0;
+			this.loopIsRunning = true;
+			do
+			{
+			switch (this.menuChoise)
+			{
+				default:
+					Console.Clear();
+					Console.WriteLine("1: Create User");
+					Console.WriteLine("2: Login User");
+					Console.WriteLine("3: Return to Previous Menu");
 
-                    int.TryParse(Console.ReadLine(), out menuChoise);
-                    //simple if to check that the number is corrisponding to a 'Menu Item'
-                    if (menuChoise == 1 || menuChoise == 2 || menuChoise == 3)  // ** expand with the numbers of the menu items
-                    {
-                        isAnInt = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Wrong input! , Can't find a matching menu item!");
-                    }
-                    break;
-                case 1:
-                    // Creation of user , ouput from system handled inside respective class that handles the stuff
-                    menuChoise = 0;
-                    Console.WriteLine("Welcome to User Creation Service");
-                    startMenu();
-                    break;
-                case 2:
-                    // Login of users
-                    menuChoise = 0;
-                    Console.WriteLine("Welcome to User Login service");
-                    startMenu();
-                    break;
-                case 3:
-                    Console.Clear();
-                    Console.WriteLine("Returning to previous menu..");
-                    Thread.Sleep(1200);
-                    startMenu();
-                    break;
-            }
-            } while (loopIsRunning);
-        }
-    }
+					int.TryParse(Console.ReadLine(), out this.menuChoise);
+					//simple if to check that the number is corrisponding to a 'Menu Item'
+					if (this.menuChoise == 1 || this.menuChoise == 2 || this.menuChoise == 3)  // ** expand with the numbers of the menu items
+					{
+						this.isAnInt = true;
+					}
+					else
+					{
+						Console.WriteLine("Wrong input! , Can't find a matching menu item!");
+					}
+						break;
+				case 1:
+					// Creation of user , ouput from system handled inside respective class that handles the stuff
+					this.menuChoise = 0;
+					Console.WriteLine("Welcome to User Creation Service");
+					startMenu();
+					 break;
+				case 2:
+					// Login of users
+					this.menuChoise = 0;
+					Console.WriteLine("Welcome to User Login service");
+					startMenu();
+					 break;
+				case 3:
+					Console.Clear();
+					Console.WriteLine("Returning to previous menu..");
+					Thread.Sleep(1200);
+					startMenu();
+					this.loopIsRunning = false;
+					 break;
+			}
+			} while (this.loopIsRunning);
+		}
+	}
 }
