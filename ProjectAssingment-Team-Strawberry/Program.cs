878c56e3f0863d622cbca5c8e0851cb386b7b563
Daniel Svensson <daniel.svensson682@gmail.com>
2022-11-07 21:23:11 +0100

    small start on user interface

--- ProjectAssingment-Team-Strawberry/Program.cs
+++ ProjectAssingment-Team-Strawberry/Program.cs
@@ -6,7 +6,30 @@ namespace ProjectAssingment_Team_Strawberry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // creating a userInterface with a Switch case
+            int Choise = 0;
+            switch (Choise)
+            {
+                default: // default is user's choices, writeline messages with the choices available.
+
+                    Console.WriteLine("1: User management");
+
+                    Choise = 0;
+                    break;
+                case 1:
+                    switch (Choise)
+                    {
+                        default:
+                            Console.WriteLine("1: Create User");
+                            Console.WriteLine("2: Login user");
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
