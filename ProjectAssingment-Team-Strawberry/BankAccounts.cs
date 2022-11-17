e26c49209085cb83a11f9c7eec530a179e9aaf51
elcode22 <elias.hammou@edu.edugrade.se>
2022-11-17 16:55:39 +0100

    changes

--- ProjectAssingment-Team-Strawberry/BankAccounts.cs
+++ ProjectAssingment-Team-Strawberry/BankAccounts.cs
@@ -6,35 +6,89 @@ namespace ProjectAssingment_Team_Strawberry
 {
     class BankAccounts : Userhandling
     {
-        public string account;
+        public string account; // toUpper or toLower
         public double balance;
-        public string accountType;
-        public string currencyType;
+        public string accountType; // toUpper or toLower
+        public string currencyType; // toUpper or toLower
         public float intrestRate;
 
         List<string> transactionLog = new List<string>();
+        
 
-        Userhandling uh = new Userhandling();
-        //
-
-        public void createBankAccounts()
+        public void createBankAccounts(List<Userhandling> Users)
         {
-            uh.CreateUser();
+            string tempUserName;
+            Console.WriteLine("Enter the UserName of the accountHolder");
+            tempUserName = Console.ReadLine();
+
+            foreach (Userhandling user in Users.FindAll(un => un.userName == tempUserName))
+            { 
+            BankAccounts newAccount = new BankAccounts();
+
+            Console.WriteLine("Please enter the account Number");
+
+            newAccount.account = Console.ReadLine();
+
+            user.MyAccounts.Add(newAccount);
+
+                
+            }
+
         }
-        public bool transferMoney(string account1, string account2)
+        public void transferMoney(string account1, string account2)
         {
+
+            MyAccounts.FindAll("hitta första kontot");
+
+            double tempvalue; // plocka bort för att ge till andra kontot
+
+            MyAccounts.FindIndex("konto nummer 2");
+            MyAccounts["konto"] += tempvalue;
 
         }
-        public bool transferAccountToAnother()
+        public void transferAccountToAnother(Userhandling user1, Userhandling user2)
         {
 
+         foreach(BankAccounts account in user1.MyAccounts.FindAll(acc => acc.account == "Name")) 
+            {
+
+
+                foreach (BankAccounts account2 in user2.MyAccounts.FindAll(acc2 => acc2.account == "Name"))
+                {
+
+                    double tempBalance; // + - transaction
+
+
+                    tempBalance -= account.balance;
+
+                    if (account.balance < 0 ) 
+                    {
+                        tempBalance += account.balance;
+                        Console.WriteLine("NOT ENOUGH MONEY! EXTERMINATE!");
+                        return;
+                    }
+
+                    account.transactionLog.Add($" Amount was removed {tempBalance - account.balance}");
+                    tempBalance += account2.balance;
+                    account2.transactionLog.Add($" Amount was removed {tempBalance - account2.balance}");
+
+                }
+
+            }
+
+            
         }
         public void currencyConverter(string currency, string currency2, double exchangeRate)
         {
 
         }
-        public bool logTransaction(List<string> tLog, double amount)
+        public void ShowlogTransaction(List<BankAccounts> accounts)
         {
+
+            foreach (BankAccounts account in accounts) 
+            {
+                
+            }
 
         }
 
